f24ccfdd1766698e3a29dd7f7c6e381809aabcdb
Abyssinia <abyssinianempire@gmail.com>
2015-02-24 00:40:32 -0600

    enable building

--- lib/Lacuna/DB/Result/Building/Permanent/MassadsHenge.pm
+++ lib/Lacuna/DB/Result/Building/Permanent/MassadsHenge.pm
@@ -6,30 +6,10 @@ no warnings qw(uninitialized);
 extends 'Lacuna::DB::Result::Building::Permanent';
 use Lacuna::Util qw(randint);
 
-#with "Lacuna::Role::Building::UpgradeWithHalls";
-#with "Lacuna::Role::Building::CantBuildWithoutPlan";
+with "Lacuna::Role::Building::UpgradeWithHalls";
+with "Lacuna::Role::Building::CantBuildWithoutPlan";
 
 use constant controller_class => 'Lacuna::RPC::Building::MassadsHenge';
-
-# When enabled, delete the following around can_build and can_upgrade
-# TODO
-around can_build => sub {
-    my ($orig, $self, $body) = @_;
-    confess [1013,"You can't build Massad's Henge."];
-    if ($body->get_plan(__PACKAGE__, 1)) {
-        return $orig->($self, $body);  
-    }
-    confess [1013,"You can't build Massad's Henge."];
-};
-
-around can_upgrade => sub {
-    my ($orig, $self) = @_;
-    confess [1013,"You can't upgrade Massad's Henge. It was left behind by the Great Race."];
-    if ($self->body->get_plan(__PACKAGE__, $self->level + 1)) {
-        return $orig->($self);  
-    }
-    confess [1013,"You can't upgrade Massad's Henge. It was left behind by the Great Race."];
-};
 
 use constant image => 'massadshenge';
 
